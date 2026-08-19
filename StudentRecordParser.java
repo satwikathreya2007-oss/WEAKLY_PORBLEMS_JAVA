1d1f9e3d3ccfbdd4ea4f600ba4c8f95ac1101148
Satwik Athreya <satwikathreya2007@gmail.com>
2026-08-19 23:34:02 +0530

    Implement StudentRecordParser to parse CSV student records
    This class parses a CSV line representing a student record and prints the details if the record is valid.

---
+++ StudentRecordParser.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class StudentRecordParser {
+
+    static void parseStudentRecord(String csvLine) {
+
+        String[] fields = csvLine.split(",");
+
+        if (fields.length != 3) {
+            System.out.println("Invalid Record");
+            return;
+        }
+
+        String name = fields[0];
+        String rollNumber = fields[1];
+        String department = fields[2];
+
+        System.out.println("Name: " + name +
+                           " | Roll No: " + rollNumber +
+                           " | Dept: " + department);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String csvLine = sc.nextLine();
+
+        parseStudentRecord(csvLine);
+    }
+}
